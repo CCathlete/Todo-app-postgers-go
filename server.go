a64c507ba588d5cb9c6f61fbaaee3966e2a6d1e7
CCat <ccathlete01@gmail.com>
2024-04-21 12:43:06 +0300

    Added support for html template using gofiber.

--- server.go
+++ server.go
@@ -6,33 +6,39 @@ import (
 	"log"
 	"os"
 
-	"github.com/gofiber/fiber"
+	fiber "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
 	connStr := "postgresql://postgres:postgres@127.0.0.1/5432/todoapp?sslmode=disable"
 
+	engine := html.New("./views", "html")
+	configuration := fiber.Config{
+		Views: engine,
+	}
+	app := fiber.New(configuration)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	app := fiber.New()
+	app = fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) {
-		indexHandler(c, db)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
 	})
 
-	app.Post("/", func(c *fiber.Ctx) {
-		postHandler(c, db)
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
 	})
 
-	app.Put("/update", func(c *fiber.Ctx) {
-		putHandler(c, db)
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
 	})
 
-	app.Delete("/delete", func(c *fiber.Ctx) {
-		deleteHandler(c, db)
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
 	})
 
 	port := os.Getenv("PORT")
@@ -44,18 +50,32 @@ func main() {
 	log.Fatalln(err)
 }
 
-func indexHandler(c *fiber.Ctx, db *sql.DB) {
-	c.SendString("Hemlo")
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res string
+	var todos []string
+	rows, err := db.Query("SELECT * FROM todos")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("An error occured.")
+	}
+	for rows.Next() {
+		rows.Scan(&res)
+		todos = append(todos, res)
+	}
+	return c.Render("index", fiber.Map{
+		"Todos": todos,
+	})
 }
 
-func postHandler(c *fiber.Ctx, db *sql.DB) {
-	c.SendString("Hemlo")
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hemlo")
 }
 
-func putHandler(c *fiber.Ctx, db *sql.DB) {
-	c.SendString("Hemlo")
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hemlo")
 }
 
-func deleteHandler(c *fiber.Ctx, db *sql.DB) {
-	c.SendString("Hemlo")
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hemlo")
 }
